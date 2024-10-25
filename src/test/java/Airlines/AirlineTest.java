a22d5282447ab3fa559e2c563df624f49ce6b425
Nuttan SWAIN <nuttan.swain@hexagon.com>
2024-10-25 13:10:51 +0530

    refactored code

--- src/test/java/Airlines/AirlineTest.java
+++ src/test/java/Airlines/AirlineTest.java
@@ -13,16 +13,17 @@ public class AirlineTest {
         Response response = RestAssured.given().log().all()
                 .baseUri("https://api.instantwebtools.net/v1/airlines")
                 .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"_id\":\"252d3bca-d9bb-5005d-12c4-562d685e235c\",\n" +
-                        "    \"name\": \"Hexagon Airways\",\n" +
-                        "    \"country\": \"India\",\n" +
-                        "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
-                        "    \"slogan\": \"From Hyedrabad\",\n" +
-                        "    \"head_quaters\": \"HiTechCity, iNDIA\",\n" +
-                        "    \"website\": \"www.hexagonairways.com\",\n" +
-                        "    \"established\": \"2024\"\n" +
-                        "}")
+                .body("""
+                        {
+                            "_id":"252d3bca-d9bb-5005d-12c4-562d685e235c",
+                            "name": "Hexagon Airways",
+                            "country": "India",
+                            "logo": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
+                            "slogan": "From Hyedrabad",
+                            "head_quaters": "HiTechCity, iNDIA",
+                            "website": "www.hexagonairways.com",
+                            "established": "2024"
+                        }""")
                 .post()
                 .then().log().all().extract().response();
         Assert.assertEquals(response.getStatusCode(), 200);
